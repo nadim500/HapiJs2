3b5fd2c83c64da8847d757c11f198c11d373850b
nadim500 <duivan5@gmail.com>
2016-02-01 15:17:21 +0000

    handlebars

---
+++ server.js
@@ -0,0 +1,31 @@
+var Hapi = require('hapi');
+var Vision = require('vision');
+var Path = require('path');
+
+var server = new Hapi.Server();
+
+server.connection({
+    host:'localhost',
+    port:Number(process.argv[2]||8080)
+});
+
+server.register(Vision,function(){});
+
+server.views({
+    engines:{
+        html:require('handlebars')
+    },
+    path:Path.join('__dirname','templates')
+});
+
+server.route({
+    method:'GET',
+    path:'/',
+    handler:{
+        view:'index.html'
+    }
+});
+
+server.start(function(){
+    console.log("servidor corriendo en: ",server.info.uri);
+});
